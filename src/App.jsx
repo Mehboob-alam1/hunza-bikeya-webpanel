5feaa5e140100b78d085d13914c911616c21528c
= <=>
2023-04-12 13:49:25 +0500

    Committed Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import Sidebar from "./components/Sidebar/Sidebar"
 
 function App() {
 
   return (
-    <div className="bg-red-500 color-white font-large text-2xl rounded p-2">
-      Hello guys!!
-      Hope you are doing well.
+    <div>
+      <Sidebar />
     </div>
   )
 }
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <section className="w-52 h-screen bg-green-500">
+      <h2 className="text-center pt-12 text-lg font-bold">Hunza Bikeya</h2>{" "}
+    </section>
+  );
+};
+
+export default Sidebar;
