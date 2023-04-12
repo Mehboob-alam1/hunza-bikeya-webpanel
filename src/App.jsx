6a37aa10af4b7bc7cabf89b0adfd186beaac8238
sarirkarim <sarirkarim755@gmail.com>
2023-04-12 23:33:36 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
 
   return (
-    <div className="App">
+    <div className="App bg-red-500 color-white font-large text-2xl rounded p-2">
       Hello guys
     </div>
   )
