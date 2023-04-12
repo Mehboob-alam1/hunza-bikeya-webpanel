ecbb6d8544ca3486beec6b4ebabedde09dcdb0cb
= <=>
2023-04-12 11:46:08 +0500

    commited-changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-import './App.css'
 
 function App() {
 
   return (
-    <div className="App bg-red-500 color-white font-large text-2xl rounded p-2">
-      Hello guys
+    <div className="bg-red-500 color-white font-large text-2xl rounded p-2">
+      Hello guys!!
     </div>
   )
 }
