0af54686d27db68d950e46a521272333d0ce0dc0
sarirkarim <sarirkarim755@gmail.com>
2023-04-19 01:10:47 +0500

    add logo

--- src/auth/register/Register.jsx
+++ src/auth/register/Register.jsx
@@ -3,12 +3,13 @@ import "./Register.css";
 import { AiOutlineTwitter, AiOutlineInstagram } from "react-icons/ai";
 import { FaPinterest, FaFacebookF, FaGithub } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import logo from '../../assets/logo.png'
 
 const Register = () => {
   return (
     <div className="background relative">
       <div className="bg-grad p-10">
-        <img src="" alt="" className="w-28 " />
+        <img src={logo} alt="" className="w-28 " />
 
         <div className="w-2/6 bg-grad1 ml-auto h-auto rounded-lg  pt-8 pl-8 pr-8 absolute right-52 top-20  pb-10">
           <div className="flex justify-between">
