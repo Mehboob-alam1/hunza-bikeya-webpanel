2db8dfd68e8be0454cb054ca2bee6021ceb6883b
sarirkarim <sarirkarim755@gmail.com>
2023-04-21 23:49:16 +0500

    change commit

---
+++ src/pages/Drivers/DriversData.js
@@ -0,0 +1,66 @@
+import img1 from '../../assets/passengerImages/table1.png'
+import img2 from '../../assets/passengerImages/table2.png'
+import img3 from '../../assets/passengerImages/table3.png'
+import img4 from '../../assets/passengerImages/table4.png'
+import img5 from '../../assets/passengerImages/table5.png'
+import img6 from '../../assets/passengerImages/table6.png'
+
+export const DriversData = [
+    {
+        img: img1,
+        name: "Earrly Casig",
+        email: "earlyca@gmail.com",
+        phone: "875-9496-66",
+        trips: "172",
+        cancel: "6",
+        status: "Offline",
+        
+    },
+    {
+        img: img2,
+        name: "Earrly Casig",
+        email: "earlyca@gmail.com",
+        phone: "875-9496-66",
+        trips: "172",
+        cancel: "6",
+        status: "Online",
+
+    },
+    {
+        img: img3,
+        name: "Earrly Casig",
+        email: "earlyca@gmail.com",
+        phone: "875-9496-66",
+        trips: "172",
+        cancel: "6",
+        status: "Online",
+    },
+    {
+        img: img4,
+        name: "Earrly Casig",
+        email: "earlyca@gmail.com",
+        phone: "875-9496-66",
+        trips: "172",
+        cancel: "6",
+        status: "Online",
+    },
+    {
+        img: img5,
+        name: "Earrly Casig",
+        email: "earlyca@gmail.com",
+        phone: "875-9496-66",
+        trips: "172",
+        cancel: "6",
+        status: "Online",
+    },
+    {
+        img: img6,
+        name: "Earrly Casig",
+        email: "earlyca@gmail.com",
+        phone: "875-9496-66",
+        trips: "172",
+        cancel: "6",
+        status: "Online",
+    },
+    
+]
---
+++ src/pages/Passengers/Passengers.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { GrNotification } from "react-icons/gr";
+import { BsChevronDown } from "react-icons/bs";
+import { CgChevronDown } from "react-icons/cg";
+import profile from "../../assets/passengerImages/profile.png";
+import "./Passenger.css";
+import { AiOutlineSearch } from "react-icons/ai";
+import { PassengerData } from "./PassengerData";
+
+const Passengers = () => {
+  return (
+    <div className=" w-10/12 pl-5 pr-6 pt-6 bg-gray-50">
+      <div className="flex justify-between ">
+        <span className="text-3xl">Passengers</span>
+
+        <div className="flex items-center gap-4 ">
+          <div className="notification">
+            <GrNotification />
+          </div>
+
+          <div className="rounded-full h-6 w-6 flex items-center justify-center border-green-500 border-2">
+            <img src={profile} alt="" className="rounded-full" />
+          </div>
+          <CgChevronDown />
+        </div>
+      </div>
+      <div className="flex justify-between pt-6 items-center">
+        <div className="flex items-center gap-1 bg-white p-1 shadow rounded-sm">
+          <AiOutlineSearch className="text-xl" />
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-transparent outline-none"
+          />
+        </div>
+
+        <div className="flex items-center gap-2">
+          <span className="text-lg">Sort By:</span>
+          <select
+            id="countries"
+            className="bg-white border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-50 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none"
+          >
+            <option selected value="Trips 0 to Eng to Limit">Trips 0 to Eng to Limit</option>
+            <option value="US">United States</option>
+            <option value="CA">Canada</option>
+            <option value="FR">France</option>
+            <option value="DE">Germany</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="relative overflow-x-auto shadow-md sm:rounded-sm mt-5">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 mx-scroll">
+          <thead className="text-xs text-gray-700 uppercase bg-white dark:bg-gray-700 dark:text-gray-400">
+            <tr className="border-b border-grey-50">
+              <th scope="col" className="px-6 py-3 border-r">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-3 border-r">
+                Email
+              </th>
+              <th scope="col" className="px-6 py-3 border-r">
+                Phone
+              </th>
+              <th scope="col" className="px-6 py-3 border-r">
+                Trips
+              </th>
+              <th scope="col" className="pl-3 py-3">
+                Canceled
+              </th>
+              <th scope="col" className="px-0 py-3">
+                status
+              </th>
+              <th scope="col" className="px-0 py-3"></th>
+            </tr>
+          </thead>
+          {PassengerData.map((data, index) => (
+            <tbody key={index}>
+              <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700 border-b">
+                <td
+                  scope="row"
+                  className="pl-4 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white flex items-center gap-2"
+                >
+                  <img src={data.img} alt="" />
+                  <span>{data.name}</span>
+                </td>
+                <td className="pl-2 py-4">{data.email}</td>
+                <td className="px-2 py-4">{data.phone}</td>
+                <td className="px-4 py-4">{data.trips}</td>
+                <td className="px-4 py-4">{data.cancel}</td>
+                <td className="px-0 py-4">
+                  <button className="p-2 bg-gray-50 rounded-md">
+                    {data.status}
+                  </button>
+                </td>
+                <td className="px-2 py-4">
+                  <button className="p-1 bg-green-500 text-white rounded-md">
+                    Details
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          ))}
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Passengers;
